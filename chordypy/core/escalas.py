e12c2a9a35b9ba9e2ccf445fd986c03f97b5dfe7
Marcos Vinicius <152632160+MarcosVini365@users.noreply.github.com>
2024-04-24 20:57:54 -0300

    standard scales to a function
    I added the standard scales to a function

--- chordypy/core/escalas.py
+++ chordypy/core/escalas.py
@@ -1,1 +1,4 @@
-escala_default = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+def scale_def():
+    escala_default = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    return escala_default

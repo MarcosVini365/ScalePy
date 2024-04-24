7c53e59abcaa76e7342483c7080cbab19f6f61a2
Marcos Vinicius <152632160+MarcosVini365@users.noreply.github.com>
2024-04-24 20:59:33 -0300

    relocated code
    Main code relocated into a new function

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-        # 0    1     2    3     4    5    6     7    8     9    10   11
+import chordypy.core.gerador_escalas as gs
 
-escala_maior = [2,2,1,2,2,2]
-escala_menor = [2,1,2,2,1,2]
+if __name__ == '__main__':
+    while True:
+        tonica = str(input("Tônica: "))
+        tonalidade = str(input('Tonalidade: '))
 
-while True:
-    tom = input('Acorde: ').upper()
-    pos = notes.index(tom)
-
-    nota_natural = [tom]
-    for i in escala_maior:
-        pos += i
-        if pos >= len(notes):
-            pos -= len(notes)
-
-        if i == 1:
-            nota_atual = notes[pos]
-            nota_anterior = notes[pos - i]
-
-            if nota_anterior in nota_atual:
-                nota_natural.append(notes[pos] + "/" + notes[pos + 1] + "b")
-            else:
-                nota_natural.append(notes[pos])
-        else:
-            nota_natural.append(notes[pos])
-    print(nota_natural)
+        gs.gerar_escala(tonica, tonalidade)

e36269fd22364e369ce39930071d9df506096f59
Marcos Vinicius <152632160+MarcosVini365@users.noreply.github.com>
2024-04-24 15:54:00 -0300

    index

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
 notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
         # 0    1     2    3     4    5    6     7    8     9    10   11
 
-n = {'C': 0, 'C#': 0.5, "D": 1, "D#": 1.5, "E": 2, "F": 2.5, "F#": 3, "G": 3.5, "G#": 4, "A": 4.5, "A#": 5, "B": 5.5}
+escala_maior = [2,2,1,2,2,2]
+escala_menor = [2,1,2,2,1,2]
 
-
-escala_maior = [2,2,1,2,2,2,1]
-tom = 'C'
-pos = notes.index(tom)
-
-indice = n[n]
-
-for intervalo in escala_maior:
-    # Incrementar o índice pela quantidade de semitons do intervalo
-    indice += intervalo
-    # Se o índice ultrapassar 11 (o número total de notas cromáticas), voltamos ao início da lista
-    indice = indice % 12
-    # Adicionar a próxima nota da escala à lista
-    for key, value in n.items():
-        if value == indice:
-            escala.append(key)
-            break
-
-'''while True:
-
+while True:
     tom = input('Acorde: ').upper()
     pos = notes.index(tom)
 
@@ -32,13 +14,14 @@ for intervalo in escala_maior:
         if pos >= len(notes):
             pos -= len(notes)
 
-        nota_natural.append(notes[pos])
+        if i == 1:
+            nota_atual = notes[pos]
+            nota_anterior = notes[pos - i]
 
-
-        ultima_nota = [nota_natural[-2], nota_natural[-1]]
-
-        if ultima_nota[0] in ultima_nota[1]:
-            nota_natural[-1] = notes[pos + 1] + "b"
-
-
-    print(nota_natural)'''
+            if nota_anterior in nota_atual:
+                nota_natural.append(notes[pos] + "/" + notes[pos + 1] + "b")
+            else:
+                nota_natural.append(notes[pos])
+        else:
+            nota_natural.append(notes[pos])
+    print(nota_natural)
